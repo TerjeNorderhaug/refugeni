5faf0e4aa8b11de80abfbefc09784576d2343fda
Terje Norderhaug <terje@in-progress.com>
2015-08-13 16:00:57 -0700

    factorize out jokes

---
+++ src/cljs/server/core.cljs
@@ -0,0 +1,32 @@
+(ns server.core
+  (:require-macros [cljs.core.async.macros :as m :refer [go go-loop alt!]])
+  (:require [cljs.nodejs :as nodejs]
+            [cljs.core.async :as async :refer [chan close! timeout put! map<]]
+            [clojure.string :as string]
+            [server.jokes :refer [fetch-some-jokes]]))
+
+(enable-console-print!)
+
+(def express (nodejs/require "express"))
+
+(defn handler [req res]
+  (let [jokes-chan (fetch-some-jokes 500)]
+    (go-loop [content nil]
+      (let [joke (<! jokes-chan)]
+        (if (some? joke)
+          (recur (cons joke content))
+          (do
+            (.set res "Content-Type" "text/plain")
+            (.send res (string/join "\n\n" content))))))))
+
+(defn server [handler port cb]
+  (let [app (express)]
+    (.get app "/" handler)
+    (.listen app port cb)))
+
+(defn -main [& mess]
+  (let [port (or (.-PORT (.-env js/process)) 1337)]
+    (server handler port
+            #(println (str "Server running at http://127.0.0.1:" port "/")))))
+
+(set! *main-cli-fn* -main)
--- src/cljs/server/jokes.cljs
+++ src/cljs/server/jokes.cljs
@@ -1,14 +1,10 @@
-(ns server
+(ns server.jokes
   (:require-macros [cljs.core.async.macros :as m :refer [go go-loop alt!]])
   (:require [cljs.nodejs :as nodejs]
             [cljs.core.async :as async :refer [chan close! timeout put! map<]]
             [clojure.string :as string]))
 
-(enable-console-print!)
-
 (def http (nodejs/require "http"))
-
-(def express (nodejs/require "express"))
 
 (defn fetch-some-joke
   "Fetch joke from The Internet Chuck Norris Database"
@@ -22,7 +18,7 @@
     c))
 
 (defn fetch-some-jokes
-  "Fetch some jokes from The Internet Chuck Norris Database up to given limit"
+  "Fetch some jokes from The Internet Chuck Norris Database up to given char limit"
   [max-len]
   (let [c (chan)]
     (go
@@ -33,25 +29,3 @@
             (recur (+ len (count joke))))
           (close! c))))
     c))
-
-(defn handler [req res]
-  (let [jokes-chan (fetch-some-jokes 500)]
-    (go-loop [content nil]
-      (let [joke (<! jokes-chan)]
-        (if (some? joke)
-          (recur (cons joke content))
-          (do
-            (.set res "Content-Type" "text/plain")
-            (.send res (string/join "\n\n" content))))))))
-
-(defn server [handler port cb]
-  (let [app (express)]
-    (.get app "/" handler)
-    (.listen app port cb)))
-
-(defn -main [& mess]
-  (let [port (or (.-PORT (.-env js/process)) 1337)]
-    (server handler port
-            #(println (str "Server running at http://127.0.0.1:" port "/")))))
-
-(set! *main-cli-fn* -main)
